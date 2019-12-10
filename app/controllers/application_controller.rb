41ece9ac09d876f66247d760c7d5e0b1e0040fcf
meghasonkusare <megha.sonkusare@anchanto.com>
2019-12-10 17:35:00 +0530

    commit changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :set_student, only: [:show, :edit, :update, :destroy]
+    def set_student
+        byebug
+        puts "from Application contorller"
+      end
+  
 end
